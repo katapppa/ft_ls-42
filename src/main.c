692f31831e47976ca6f9a9d2d7aa1fb4ea8a4990
Crius Gamora <cgamora@mi-k5.msk.21-school.ru>
2020-11-11 12:42:02 +0300

    initial commit

--- src/main.c
+++ src/main.c
@@ -6,78 +6,45 @@
 /*   By: cgamora <cgamora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 13:42:57 by cgamora           #+#    #+#             */
-/*   Updated: 2020/11/09 19:25:23 by cgamora          ###   ########.fr       */
+/*   Updated: 2020/11/11 07:19:42 by cgamora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
-void    ft_direc_error(char *s)
+t_flags		*memory_strfl(t_flags *flag)
 {
-    printf("ls: %s: No such file or directory\n", s);
-    exit(1);
+	if (!(flag = (t_flags*)malloc(sizeof(t_flags))))
+		exit(1);
+	flag->a = 0;
+	flag->l = 0;
+	flag->R = 0;
+	flag->r = 0;
+	flag->t = 0;
+	return (flag);
 }
 
-void    ft_flag_error(char **argv, int i)
+int			main(int argc, char **argv)
 {
-    if (argv[1][i] == '-')
-        ft_direc_error(&argv[1][i]);
-    ft_printf("No flag %c\n", argv[1][i]);
-    exit(1);
-}
+	t_flags			*flag;
 
-int     parse_to_struct(t_flags *flag, int argc, char **argv)
-{
-    int i;
+	flag = memory_strfl(flag);
+	parse_to_struct(flag, argc, argv);
+	ft_printf("PARSER: %d\n", g_parser);
+	ft_printf("ARGC: %d\n", argc);
+	// ft_printf("%d and %d and %d and %d and %d\n", flag->a, flag->l, flag->R, flag->r, flag->t);
+	if (g_parser == argc)
+	{
+		ft_start_ls(".", flag);
+	}
+	// else if (g_parser + 1 == argc)
+	// {
 
-    i = 1;
-    if (argv[1] && argv[1][0] == '-')
-    {
-        while (argv[1][i])
-        {
-            if (argv[1][i] == 'l')
-                flag->l = 1;
-            else if (argv[1][i] == 'a')
-                flag->a = 1;
-            else if (argv[1][i] == 'R')
-                flag->R = 1;
-            else if (argv[1][i] == 'r')
-                flag->r = 1;
-            else if (argv[1][i] == 't')
-                flag->t = 1;
-            else
-                ft_flag_error(argv, i);
-            i++;
-        }
-    }
-    return (1);
-}
-
-t_flags    *memory_strfl(t_flags *flag)
-{
-    if (!(flag = (t_flags*)malloc(sizeof(t_flags))))
-        exit(1);
-    flag->a = 0;
-    flag->l = 0;
-    flag->R = 0;
-    flag->r = 0;
-    flag->t = 0;
-    return (flag);
-}
-
-int     main(int argc, char **argv)
-{
-    t_flags *flag;
-    DIR     *folder;
-	struct dirent	*first;
-
-    flag = memory_strfl(flag);
-    parse_to_struct(flag, argc, argv);
-    ft_printf("%d and %d and %d and %d and %d\n", flag->a, flag->l, flag->R, flag->r, flag->t);
-
-    folder = opendir(".");
-	first = readdir(folder);
-	struct stat stet;
-	lstat(".", &stet);
-    return 1;
+	// }
+	// else
+	// {
+		
+	// }
+	free(flag);
+	return (1);
 }
